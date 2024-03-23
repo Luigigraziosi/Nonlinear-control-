ec3aee332e0b8a31d0a9987bd230ed90e600474d
Luigigraziosi <lgraziosi38@gmail.com>
2024-03-23 18:18:33 +0100

    Esercizio 1-2-3

--- lab1/lab1es1.m
+++ lab1/lab1es1.m
@@ -35,17 +35,14 @@ plot(time, data(:,2))
 nexttile;
 plot(time, data(:,3))
 
+figure(1)
 x0 = 0.001*randn(3,1);
 %open("chuasim")
 res = sim("chuasim");
 data = res.chuaOut.Data;
 
-figure(1)
+
 plot3(data(:,1), data(:,2), data(:,3))
-xlabel('X2')
-ylabel('Y2')
-zlabel('Z2')
-hold off
 %% Equilibrium point
 
 ep = [0,1/sqrt(2), -1/sqrt(2);0,0,0; 0,-1/sqrt(2),+1/sqrt(2) ]
@@ -72,35 +69,37 @@ eig2 = eig(A2);
 eig3 = eig(A3);
 
 disp("The all 3 equilibrium point are unstable");
+
 %% Excercise 3
 
 x0 = ep(:,3) + 0.01*randn(3,1)
 res = sim("chuasim");
 time = res.chuaOut.Time;
 data = res.chuaOut.Data;
-figure(1)
+figure(3)
 plot3(data(:,1), data(:,2), data(:,3))
-xlabel('X1')
-ylabel('Y1')
-zlabel('Z1')
+xlabel('X')
+ylabel('Y')
+zlabel('Z')
 hold on
-
-
-
-
-
-
-hold on
-x_0 = ep(:,2) + 0.01*randn(1,3)
+plot3(0,0,0, 'ro', 'MarkerSize', 3);
+x0 = ep(:,2) + 0.01*randn(3,1)
 res = sim("chuasim");
 time = res.chuaOut.Time;
 data = res.chuaOut.Data;
-figure(3)
 plot3(data(:,1), data(:,2), data(:,3))
-x_0 = ep(:,3) + 0.01*randn(1,3)
+hold on
+plot3(ep(1,2),ep(2,2),ep(3,2), 'ko', 'MarkerSize',3)
+x0 = ep(:,3) + 0.01*randn(3,1)
 res = sim("chuasim");
 time = res.chuaOut.Time;
 data = res.chuaOut.Data;
-figure(3)
 plot3(data(:,1), data(:,2), data(:,3))
+plot3(ep(1,3),ep(2,3),ep(3,3), 'co', 'MarkerSize',3)
+
+
+
+
+
+
 
